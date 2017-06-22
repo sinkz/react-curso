e1190478dc477a30ba88a0c1a106d5d030757039
Sinkz <diih_augusto@live.com>
2017-06-22 13:14:10 -0300

    commit

---
+++ exercicios_react/ex.5/goncalvesFamily.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name="Diego" lastName="Gonçalves"/>
+        <Member name="Giovana" lastName="Gonçalves"/>
+    </div>
+)
---
+++ exercicios_react/ex.5/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GoncalvesFamily from './goncalvesFamily'
+
+ReactDOM.render(
+    <div>
+        <GoncalvesFamily />
+    </div>
+    , document.getElementById('app'));
+
+
